76f9945f855cd319830fd0903d2055e196d70f86
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-24 17:47:16 -0700

    monty

--- div.c
+++ div.c
@@ -3,7 +3,17 @@
 #include "monty.h"
 
 /**
- * div_operation - Divides the second top element of
+ * div_op-import random
+import ctypes
+
+cops = ctypes.CDLL('./100-operations.so')
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b))) Divides the second top element of
  * the stack by the top element of the stack.
  * @stack: Pointer to the stack
  * @line_number: Line number in the Monty file
